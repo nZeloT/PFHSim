82a020c43fea3d547ecc20b42abf0ce45a3ea4fb
wolfmarcel <marcel@wolf4.de>
2015-12-10 14:05:53 +0100

    First UI Build

--- PFHSimClient/src/src/ui/JavaFXTest.java
+++ PFHSimClient/src/src/ui/JavaFXTest.java
@@ -1,4 +1,4 @@
-package ui;
+package src.ui;
 
 import java.io.IOException;
 import java.net.URL;
@@ -6,26 +6,29 @@ import java.net.URL;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class JavaFXTest extends Application {
-
+	
 	@Override
 	public void start(Stage primaryStage) {
-		URL location = getClass().getResource("fxml/ui_test.fxml");
+		URL location = getClass().getResource("/ui/fxml/ui_test.fxml");
 		FXMLLoader fxmlLoader = new FXMLLoader(location);
 
 		try {
-			Pane root = (Pane)fxmlLoader.load();
+			SplitPane root = (SplitPane)FXMLLoader.load(getClass().getResource("/ui/fxml/Menu.fxml"));
 			primaryStage.setScene(new Scene(root));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 		primaryStage.show();
 	}
 

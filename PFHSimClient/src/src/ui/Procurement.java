33b29103496a47f8e8fde71d3473a36e41961be3
nZeloT <leontsteiner@gmail.com>
2015-12-21 12:34:43 +0100

    initial hr ui; needs some component wiring :D

--- PFHSimClient/src/src/ui/Procurement.java
+++ PFHSimClient/src/src/ui/Procurement.java
@@ -1,31 +1,31 @@
 package src.ui;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.shape.Line;
+import javafx.scene.control.TextField;
 
-public class Procurement implements Initializable{
+public class Procurement{
+//	@FXML
+//	Line Line1;
+	
 	@FXML
-	Line Line1;
+	private TextField textWood;
 
 	public Procurement() {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
-		System.out.println(Line1);
+//	@Override
+//	public void initialize(URL arg0, ResourceBundle arg1) {
+//		System.out.println(Line1);
 		//Line1.endXProperty().bind(Line1.getScene().widthProperty().subtract(20));
 		// TODO Auto-generated method stub
 		
-	}
+//	}
+	
 	@FXML
 	private void handleBuyButton(ActionEvent event) {
-		System.out.println("button Pressed");
+		System.out.println("button Pressed" + textWood.getText());
 	}
 
 }
---
+++ PFHSimClient/src/ui/controller/hr/HRDetailsController.java
@@ -0,0 +1,13 @@
+package ui.controller.hr;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HRDetailsController {
+
+	@FXML
+	private void onBackButton(ActionEvent e){
+		
+	}
+	
+}

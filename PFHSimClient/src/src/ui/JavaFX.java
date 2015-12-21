f7599f931cc97c7da7fde5f6b402f9cad4a37d3f
nZeloT <leontsteiner@gmail.com>
2015-12-21 17:08:49 +0100

    Merge

--- PFHSimClient/src/src/ui/JavaFX.java
+++ PFHSimClient/src/src/ui/JavaFX.java
@@ -1,20 +1,14 @@
 package src.ui;
 
 import java.io.IOException;
-import java.net.URL;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import sim.Enterprise;
 import ui.hr.HRPane;
 
 public class JavaFX extends Application {

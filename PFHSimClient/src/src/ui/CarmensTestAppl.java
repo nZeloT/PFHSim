e13bba846a9c4867736e9fd8f736cd4c7fcbec04
carmabell <carmen.rannefeld@web.de>
2015-12-22 12:26:35 +0100

    finished Procurement UI

---
+++ PFHSimClient/src/src/ui/CarmensTestAppl.java
@@ -0,0 +1,68 @@
+	package src.ui;
+
+	import java.io.IOException;
+	import java.util.HashMap;
+
+	import javafx.application.Application;
+	import javafx.fxml.FXMLLoader;
+	import javafx.scene.Scene;
+	import javafx.scene.control.SplitPane;
+	import javafx.scene.layout.VBox;
+	import javafx.stage.Stage;
+	import sim.Enterprise;
+	import sim.procurement.ResourceMarket;
+	import sim.procurement.ResourceType;
+	import ui.hr.HRPane;
+
+
+public class CarmensTestAppl extends Application {
+
+
+		@Override
+		public void start(Stage primaryStage) {
+			// URL location = getClass().getResource("/ui/fxml/ui_test.fxml");
+			// FXMLLoader fxmlLoader = new FXMLLoader(location);
+
+			// try {
+			// SplitPane root =
+			// (SplitPane)FXMLLoader.load(getClass().getResource("/ui/fxml/Menu.fxml"));
+			// primaryStage.setScene(new Scene(root));
+			//primaryStage.setScene(new Scene(new HRPane(null)));
+			// } catch (IOException e) {
+			// e.printStackTrace();
+			// }
+			try {
+
+				ResourceType[] resources = ResourceType.values();
+				HashMap<ResourceType, Integer> costs = new HashMap<>();
+				for (int i = 0; i < resources.length; i++) {
+					costs.put(resources[i], resources[i].getBasePrice());
+				}
+				ResourceMarket market = new ResourceMarket(costs);
+				Enterprise e = new Enterprise(market);
+				SplitPane root = (SplitPane) FXMLLoader.load(getClass().getResource("/ui/fxml/Menu.fxml"));
+				//primaryStage.setScene(new Scene(root));
+
+				Procurement proc = new Procurement(e);
+				VBox boxP = proc.getBox();
+				// VBox boxP = (VBox)
+				// FXMLLoader.load(getClass().getResource("/ui/fxml/Procurement.fxml"));
+
+				// Warehouse ware = new Warehouse(e);
+				// VBox box = ware.getBox();
+				//primaryStage.setScene(new Scene(root));
+
+				// Warehouse ware = new Warehouse(e);
+				// VBox box = ware.getBox();
+
+				primaryStage.setScene(new Scene(boxP));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			primaryStage.show();
+		}
+
+		public static void main(String[] args) {
+			launch(args);
+		}
+	}
--- PFHSimClient/src/src/ui/Procurement.java
+++ PFHSimClient/src/src/ui/Procurement.java
@@ -1,10 +1,13 @@
 package src.ui;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import javafx.event.ActionEvent; 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -22,149 +25,200 @@ public class Procurement {
 	private @FXML Button btn_insulation;
 	private @FXML Button btn_roof;
 	private @FXML Button btn_brick;
+	private @FXML Button btn_window;
 
 	private @FXML Label price_wood;
 	private @FXML Label price_concrete;
 	private @FXML Label price_insulation;
 	private @FXML Label price_roof;
 	private @FXML Label price_brick;
-
-	private @FXML Label PLUS_wood;
-	private @FXML Label PLUS_concrete;
-	private @FXML Label PLUS_insulation;
-	private @FXML Label PLUS_roof;
-	private @FXML Label PLUS_brick;
+	private @FXML Label price_window;
 
 	private @FXML Label sum_wood;
 	private @FXML Label sum_concrete;
 	private @FXML Label sum_insulation;
 	private @FXML Label sum_roof;
 	private @FXML Label sum_brick;
+	private @FXML Label sum_window;
 
 	private @FXML Label AM_wood;
 	private @FXML Label AM_concrete;
 	private @FXML Label AM_insulation;
 	private @FXML Label AM_roof;
 	private @FXML Label AM_brick;
+	private @FXML Label AM_window;
 
 	private @FXML TextField input_wood;
 	private @FXML TextField input_concrete;
 	private @FXML TextField input_insulation;
 	private @FXML TextField input_roof;
 	private @FXML TextField input_brick;
+	private @FXML TextField input_window;
 
 	private Enterprise ent;
 	private ResourceMarket market;
 	private @FXML VBox root;
-	
+
 	private int int_amount_wood;
 	private int int_amount_concrete;
 	private int int_amount_insulation;
 	private int int_amount_roof;
 	private int int_amount_brick;
-	
-	
+	private int int_amount_window;
+
 	public Procurement(Enterprise enterprise) {
-		
+
 		FXMLLoader fxml = new FXMLLoader(getClass().getResource("/ui/fxml/Procurement.fxml"));
 		fxml.setController(this);
 		try {
-			this.ent = enterprise; 
+			this.ent = enterprise;
 			market = ent.getMarket();
-			fxml.load(); 
+			fxml.load();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} 
-	} 
- 
+		}
+	}
+
 	public void initialize() {
-		price_wood.setText(" " + market.getPrice(ResourceType.WOOD) );
+		price_wood.setText(" " + market.getPrice(ResourceType.WOOD));
 		price_concrete.setText(" " + market.getPrice(ResourceType.CONCRETE));
 		price_insulation.setText(" " + market.getPrice(ResourceType.INSULATION));
 		price_roof.setText(" " + market.getPrice(ResourceType.ROOF_TILE));
 		price_brick.setText(" " + market.getPrice(ResourceType.BRICK));
-		
+		price_window.setText(" " + market.getPrice(ResourceType.WINDOW));
+
 		AM_wood.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.WOOD));
 		AM_concrete.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.CONCRETE));
 		AM_insulation.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.INSULATION));
 		AM_roof.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.ROOF_TILE));
 		AM_brick.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.BRICK));
+		AM_window.setText("" + ent.getWarehouse().getStoredAmount(ResourceType.WINDOW));
 	}
-	
-	public void update(){
-		
-		price_wood.setText("" + market.getPrice(ResourceType.WOOD) );
+
+	public void update() {
+
+		price_wood.setText("" + market.getPrice(ResourceType.WOOD));
 		price_concrete.setText("" + market.getPrice(ResourceType.CONCRETE));
 		price_insulation.setText("" + market.getPrice(ResourceType.INSULATION));
 		price_roof.setText("" + market.getPrice(ResourceType.ROOF_TILE));
 		price_brick.setText("" + market.getPrice(ResourceType.BRICK));
+		price_window.setText(" " + market.getPrice(ResourceType.WINDOW));
+	}
+
+	public boolean checkIfNumberInput(TextField input) {
+
+		if (input.getText().matches("[0-9]{1,}")) {  
+			return true;  
+		} else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("PFH Warning");
+			alert.setHeaderText("This is a numeric input field");
+			alert.setContentText("Try again with numbers");
+
+			alert.showAndWait();
+			return false;
+		}
+
 	}
 
 	@FXML
 	private void handleBuyButton(ActionEvent event) throws EnterpriseException, ResourceMarketException {
 		Button btn = (Button) event.getSource();
-		 
+
 		if (btn.getId().equals("btn_wood")) {
-			ent.buyResources(ResourceType.WOOD, int_amount_wood);
-			AM_wood.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.WOOD));
-			System.out.println(int_amount_wood);
+			buy(AM_wood, int_amount_wood, ResourceType.WOOD);
 		}
 
 		if (btn.getId().equals("btn_insulation")) {
-			ent.buyResources(ResourceType.INSULATION, int_amount_insulation);
-			AM_insulation.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.INSULATION));
+			buy(AM_insulation, int_amount_insulation, ResourceType.INSULATION);
 		}
 
 		if (btn.getId().equals("btn_concrete")) {
-			ent.buyResources(ResourceType.CONCRETE, int_amount_concrete);
-			AM_concrete.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.CONCRETE));
+			buy(AM_concrete, int_amount_concrete, ResourceType.CONCRETE);
 		}
 		if (btn.getId().equals("btn_roof")) {
-			ent.buyResources(ResourceType.ROOF_TILE, int_amount_roof);
-			AM_roof.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.ROOF_TILE));
-
+			buy(AM_roof, int_amount_roof, ResourceType.ROOF_TILE);
 		}
 		if (btn.getId().equals("btn_brick")) {
-			ent.buyResources(ResourceType.BRICK, int_amount_brick);
-			AM_brick.setText(" " + ent.getWarehouse().getStoredAmount(ResourceType.BRICK));
+			buy(AM_brick, int_amount_brick, ResourceType.BRICK);
+		}
+
+		if (btn.getId().equals("btn_window")) {
+			buy(AM_window, int_amount_window, ResourceType.WINDOW);
 		}
 
 	}
+
 	@FXML
-	public void handleInputField(ActionEvent event){
+	public void handleInputField(ActionEvent event) {
 		TextField text = (TextField) event.getSource();
-		
-		if (text.getId().equals("input_wood")) { 
-			int_amount_wood = Integer.parseInt(text.getText());
-			sum_wood.setText(" " + market.getPrice(ResourceType.WOOD) * int_amount_wood);
-		} 
+
+		if (text.getId().equals("input_wood")) {
+			if (checkIfNumberInput(text)) {
+				int_amount_wood = Integer.parseInt(text.getText());
+				sum_wood.setText(" " + market.getPrice(ResourceType.WOOD) * int_amount_wood);
+			} else {
+				text.setText("");
+			}
+
+		}
 
 		if (text.getId().equals("input_insulation")) {
-			int_amount_insulation = Integer.parseInt(text.getText());
-			sum_insulation.setText(" " + market.getPrice(ResourceType.INSULATION)*int_amount_insulation);
+			if (checkIfNumberInput(text)) {
+				int_amount_insulation = Integer.parseInt(text.getText());
+				sum_insulation.setText(" " + market.getPrice(ResourceType.INSULATION) * int_amount_insulation);
+			} else {
+				text.setText("");
+			}
+
 		}
 
 		if (text.getId().equals("input_concrete")) {
-			int_amount_concrete = Integer.parseInt(text.getText());
-			sum_concrete.setText(" " +  market.getPrice(ResourceType.CONCRETE)*int_amount_concrete); 
+			if (checkIfNumberInput(text)) {
+				int_amount_concrete = Integer.parseInt(text.getText());
+				sum_concrete.setText(" " + market.getPrice(ResourceType.CONCRETE) * int_amount_concrete);
+			} else {
+				text.setText("");
+			}
 		}
 		if (text.getId().equals("input_roof")) {
-			int_amount_roof = Integer.parseInt(text.getText());
-			sum_roof.setText(" " + market.getPrice(ResourceType.ROOF_TILE)*int_amount_roof);
-
+			if (checkIfNumberInput(text)) {
+				int_amount_roof = Integer.parseInt(text.getText());
+				sum_roof.setText(" " + market.getPrice(ResourceType.ROOF_TILE) * int_amount_roof);
+			} else {
+				text.setText("");
+			}
 		}
 		if (text.getId().equals("input_brick")) {
-			int_amount_brick = Integer.parseInt(text.getText());
-			sum_brick.setText(" " + market.getPrice(ResourceType.BRICK)*int_amount_brick);
+			if (checkIfNumberInput(text)) {
+				int_amount_brick = Integer.parseInt(text.getText());
+				sum_brick.setText(" " + market.getPrice(ResourceType.BRICK) * int_amount_brick);
+			} else {
+				text.setText("");
+			}
 		}
-		
-	
-		
+		if (text.getId().equals("input_window")) {
+			if (checkIfNumberInput(text)) {
+				int_amount_window = Integer.parseInt(text.getText());
+				sum_window.setText(" " + market.getPrice(ResourceType.WINDOW) * int_amount_window);
+			} else {
+				text.setText("");
+			}
+
+		}
+
 	}
-	
+
+	public void buy(Label fullAmount, int amount, ResourceType type)
+			throws EnterpriseException, ResourceMarketException {
+
+		ent.buyResources(type, amount);
+
+		fullAmount.setText("" + (ent.getWarehouse().getStoredAmount(type)) + (amount));
+	}
+
 	public VBox getBox() {
 		return root;
 	}
-
 
 }

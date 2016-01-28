7006b57789df245a9951741c63fd835f986556e3
Florian <fbunsmann@web.de>
2016-01-28 13:46:26 +0100

    TestSuiteAll added

---
+++ PFHSimTest/src/TestSuiteAllTests.java
@@ -0,0 +1,34 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import sim.EnterpriseTest;
+import sim.IntegrationTest;
+import sim.hr.EmployeeTest;
+import sim.hr.HRTest;
+import sim.procurement.ResourceMarketTest;
+import sim.production.TestMachine;
+import sim.research.dev.UpgradeTest;
+import sim.simulation.sales.CheapBuyerTest;
+import sim.simulation.sales.ExpensiveBuyerTest;
+import sim.simulation.sales.PricePerformanceBuyerTest;
+import sim.warehouse.TestWarehouse;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ScenarioTest1.class,
+				ScenarioTest2.class,
+				EmployeeTest.class,
+				HRTest.class,
+				TestWarehouse.class,
+				TestMachine.class,
+				ResourceMarketTest.class,
+				EnterpriseTest.class,
+				UpgradeTest.class,
+				IntegrationTest.class,
+				ResourceMarketTest.class,
+				ExpensiveBuyerTest.class,
+				CheapBuyerTest.class,
+				PricePerformanceBuyerTest.class})
+public class TestSuiteAllTests {
+
+}
